f5b35bd0310009a837af533e96d26462cb9f9812
wbojczuk <94025275+wbojczuk@users.noreply.github.com>
2022-08-09 09:39:00 -0400

    Update animationstorm.js
    Updated yk

--- animationstorm.js
+++ animationstorm.js
@@ -8,6 +8,8 @@ const animationStorm = {
     //  DEFAULT TIME TO IDLE PLACEHOLDER AFTER ANIMATION. Leave null for infinite idling;
      simpleWriteIdle: 2000,
 
+     simpleWritePlaceholder: true,
+
 
 
     setup: ()=>{
@@ -18,67 +20,81 @@ const animationStorm = {
             const writeElems = document.querySelectorAll(".as-write-load");
             const writeElemsLength = writeElems.length;
 
-            const spanInner = document.createElement("span");
-            // const spanText = document.createElement("span");
-            const placeholderSpan = document.createElement("span");
-
-            placeholderSpan.setAttribute("class", "as-write-placeholder");
-            spanInner.setAttribute("class", "as-write-inner");
-
             for(let i=0; i<writeElemsLength; i++){
                 const currentElem = writeElems[i];
-                let currentDelay = animationStorm.simpleWriteDelay;
-                    if(currentElem.hasAttribute("data-delay")){
-                        currentDelay = currentElem.dataset.delay;
-                    }
-                let currentIdle = animationStorm.simpleWriteIdle;
-                if(currentElem.hasAttribute("data-idle")){
-                    currentIdle = currentElem.dataset.idle;
-                }
-                let tempText = currentElem.textContent;
-                currentElem.innerHTML = "&nbsp;";
-
-                currentElem.append(spanInner.cloneNode(false))
-                const currentElemInner = currentElem.querySelector(".as-write-inner");
-                const currentElemText = currentElemInner.querySelector(".as-write-text");
-                currentElemInner.append(placeholderSpan.cloneNode(false));
-                const currentPlaceholder = currentElemInner.querySelector(".as-write-placeholder");
-                // USED TO INITIALIZE ITERATION COUNTER
-                let placeholderTimer = 0;
-                currentPlaceholder.onanimationiteration = ()=>{placeholderTimer++;};
-
-               const textSpan = document.createElement("span");
-               textSpan.className = "as-text-fade";
-
-                currentElem.style.opacity = "100";
-
-                let textCount = 0;
-                setTimeout(writeLoadLoop, currentDelay);
-                
-                    function writeLoadLoop(){
-                        let currentText = textSpan.cloneNode("false");
-                        currentText.textContent = tempText.charAt(textCount);
-                        currentElemInner.insertBefore(currentText, currentElemInner.lastChild) 
-                        
-                        textCount++;
-                        if(textCount<tempText.length){
-                            setTimeout(writeLoadLoop, Math.random() * (animationStorm.simpleWriteSpeed - animationStorm.simpleWriteSpeed - 10) + animationStorm.simpleWriteSpeed - 10);
-                        } else {
-                            if(animationStorm.simpleWriteIdle){
-                                setTimeout(()=>{
-                                    currentPlaceholder.addEventListener("animationiteration", ()=>{currentPlaceholder.remove();})
-                                    }, currentIdle);
-                            }
-                        }
-                    }
-                
-
+                animationStorm.simpleWrite(currentElem)
                 
             }
         } 
         // END ON LOAD WRITE
+        setTimeout(()=>{
+            animationStorm.simpleWrite(document.getElementById("yessir"))
+        },1000)
 
-    }
+    },
+
+    simpleWrite: (currentElem)=>{
+        const spanInner = document.createElement("span");
+        // const spanText = document.createElement("span");
+        const placeholderSpan = document.createElement("span");
+
+        placeholderSpan.setAttribute("class", "as-write-placeholder");
+        spanInner.setAttribute("class", "as-write-inner");
+
+        // SETTINGS
+        let placeholderSettings = animationStorm.simpleWritePlaceholder;
+        if(currentElem.hasAttribute("data-placeholder")){
+            placeholderSettings = currentElem.dataset.placeholder;
+        }
+
+        let currentDelay = animationStorm.simpleWriteDelay;
+            if(currentElem.hasAttribute("data-delay")){
+                currentDelay = currentElem.dataset.delay;
+            }
+        let currentIdle = animationStorm.simpleWriteIdle;
+        if(currentElem.hasAttribute("data-idle")){
+            currentIdle = currentElem.dataset.idle;
+        }
+        let tempText = currentElem.textContent;
+        currentElem.innerHTML = "&nbsp;";
+
+        currentElem.append(spanInner.cloneNode(false))
+        const currentElemInner = currentElem.querySelector(".as-write-inner");
+        const currentElemText = currentElemInner.querySelector(".as-write-text");
+        currentElemInner.append(placeholderSpan.cloneNode(false));
+        const currentPlaceholder = currentElemInner.querySelector(".as-write-placeholder");
+
+        if(placeholderSettings === false || placeholderSettings == "false"){currentPlaceholder.style.display = "none";}
+
+        // USED TO INITIALIZE ITERATION COUNTER
+        let placeholderTimer = 0;
+        currentPlaceholder.onanimationiteration = ()=>{placeholderTimer++;};
+
+       const textSpan = document.createElement("span");
+       textSpan.className = "as-text-fade";
+
+        currentElem.style.opacity = "100";
+
+        let textCount = 0;
+        setTimeout(writeLoadLoop, currentDelay);
+        
+            function writeLoadLoop(){
+                let currentText = textSpan.cloneNode("false");
+                currentText.textContent = tempText.charAt(textCount);
+                currentElemInner.insertBefore(currentText, currentElemInner.lastChild) 
+                
+                textCount++;
+                if(textCount<tempText.length){
+                    setTimeout(writeLoadLoop, Math.random() * (animationStorm.simpleWriteSpeed - animationStorm.simpleWriteSpeed - 10) + animationStorm.simpleWriteSpeed - 10);
+                } else {
+                    if(animationStorm.simpleWriteIdle){
+                        setTimeout(()=>{
+                            currentPlaceholder.addEventListener("animationiteration", ()=>{currentPlaceholder.remove();})
+                            }, currentIdle);
+                    }
+                }
+            }
+    },
 };
 
 animationStorm.setup();
